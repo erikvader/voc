72a5e51972af3f8b469cb797603c4ccf99c728d0
Dennis Englund <dennisenglund88@live.com>
2020-09-16 13:20:51 +0200

    Fixed style
    Co-Authored-By: ErikKrantz <25820542+ErikKrantz@users.noreply.github.com>

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -1,36 +1,35 @@
 package python;
 
 @org.python.Module(
-  __doc__ = "A module that supplies some mathematical functions.\n"+
-            "Current functions:\n"+
-            "pow(x, y) -> Return x raised to the power y."
+        __doc__ = "A module that supplies some mathematical functions.\n"+
+                    "Current functions:\n"+
+                    "pow(x, y) -> Return x raised to the power y."
 )
 
 public class math extends org.python.types.Module {
-  public math() {
-    super();
-  }
-
-  @org.python.Method(
-    __doc__ = "Returns x raised to the power y.\n",
-    args = {"x", "y"}
-  )
-
-  public static org.python.Object pow(org.python.Object x, org.python.Object y) {
-
-    if(!(x instanceof org.python.types.Float || y instanceof org.python.types.Float)) {
-      throw new org.python.exceptions.TypeError("x and y needs to be of type float");
+    public math() {
+        super();
     }
 
-    double a = ((org.python.types.Float) x).value;
-    double b = ((org.python.types.Float) y).value;
+    @org.python.Method(
+        __doc__ = "Returns x raised to the power y.\n",
+        args = {"x", "y"}
+    )
 
-    if(a < 0 || b < 0) {
-      throw new org.python.exceptions.TypeError("Only values of x >= 0 and y >= 0 are accepted");
+    public static org.python.Object pow(org.python.Object x, org.python.Object y) {
+
+        if(!(x instanceof org.python.types.Float || y instanceof org.python.types.Float)) {
+            throw new org.python.exceptions.TypeError("x and y needs to be of type float");
+        }
+
+        double a = ((org.python.types.Float) x).value;
+        double b = ((org.python.types.Float) y).value;
+
+        if(a < 0 || b < 0) {
+            throw new org.python.exceptions.TypeError("Only values of x >= 0 and y >= 0 are accepted");
+        }
+
+        double result = java.lang.Math.pow(a, b);
+        return new org.python.types.Float(result);
     }
-
-    double result = java.lang.Math.pow(a, b);
-    return new org.python.types.Float(result);
-
-  }
 }
