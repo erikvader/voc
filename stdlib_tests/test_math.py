468bd59cdb2874493bb0686f72097985d44c0e73
Dennis Englund <dennisenglund88@live.com>
2020-09-16 10:48:46 +0200

    Update test_math.py
    Co-Authored-By: ErikKrantz <25820542+ErikKrantz@users.noreply.github.com>

--- stdlib_tests/test_math.py
+++ stdlib_tests/test_math.py
@@ -1,29 +1,8 @@
-import unittest
+import math
 
-from ..utils import TranspileTestCase
-
-class MathModuleTests(TranspileTestCase):
-    def test_pow(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.pow(2.0, 2.0))
-            print(math.pow(0.0, 2.0))
-            print(math.pow(2.0, 0.0))
-            print(math.pow(0.0, 0.0))
-            print(math.pow(1.0, 2.0))
-            print(math.pow(2.0, 1.0))
-        """)
-
-    @unittest.expectedFailure
-    def test_pow_with_negative(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.pow(-2.0, 2.0)))
-        """)
-
-    @unittest.expectedFailure
-    def test_pow_with_string(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.pow("string", 2.0))
-        """)
+print(math.pow(2.0, 2.0))
+print(math.pow(0.0, 2.0))
+print(math.pow(2.0, 0.0))
+print(math.pow(0.0, 0.0))
+print(math.pow(1.0, 2.0))
+print(math.pow(2.0, 1.0))
