b2c200c5625e51b3a816de067e9b5b171f8d8e95
Erik Rimskog <erik.rimskog@gmail.com>
2020-09-16 17:56:43 +0200

    fabs works on all types that has __float__

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -4,25 +4,34 @@ package python;
     __doc__ = "This module provides various functions to do math.\n" +
               "fabs(x) -- return absolute value of x"
 )
-
 public class math extends org.python.types.Module {
-    public math() {
-        super();
-    }
+
+    @org.python.Attribute()
+    public static org.python.Object __file__ = new org.python.types.Str("python/common/python/math.java");
+    @org.python.Attribute()
+    public static org.python.Object __name__ = new org.python.types.Str("math");
+    @org.python.Attribute()
+    public static org.python.Object __package__ = new org.python.types.Str("");
 
     @org.python.Method(
         __doc__ = "Returns the absolute value of the float x",
         args = {"x"}
     )
     public static org.python.Object fabs(org.python.Object x) {
-        if (!(x instanceof org.python.types.Float)) {
-            throw new org.python.exceptions.TypeError("an float is required (got type " + x.typeName() +")");
+        org.python.Object f;
+        try {
+            f = x.__float__();
+        } catch (org.python.exceptions.AttributeError e) {
+            throw new org.python.exceptions.TypeError("must be a real number, not " + x.typeName());
         }
 
-        double abs = ((org.python.types.Float) x).value;
-        if (abs < 0) {
-            abs *= -1;
+        if (!(f instanceof org.python.types.Float)) {
+            throw new org.python.exceptions.TypeError(
+                x.typeName() + ".__float__ returned a non-float (type " + f.typeName() + ")"
+            );
         }
+
+        double abs = java.lang.Math.abs(((org.python.types.Float) f).value);
         return new org.python.types.Float(abs);
     }
 }
--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -7,21 +7,24 @@ class MathModuleTests(TranspileTestCase):
     def test_fabs(self):
         self.assertCodeExecution("""
             import math
-            print(math.fabs(2.0))
+            print(math.fabs(2))
             print(math.fabs(-2.0))
-            print(math.fabs(0.0))
+            print(math.fabs(0))
             """)
 
     @unittest.expectedFailure
-    def test_fabs_wrong_arg_string(self):
+    def test_fabs_non_float_arg(self):
         self.assertCodeExecution("""
             import math
             print(math.fabs("hej"))
         """)
 
     @unittest.expectedFailure
-    def test_fabs_wrong_arg_int(self):
+    def test_fabs_wrong_float_impl(self):
         self.assertCodeExecution("""
             import math
-            print(math.fabs(0))
+            class Testtest:
+                def __float__():
+                    return "not float"
+            print(math.fabs(Testtest()))
         """)
