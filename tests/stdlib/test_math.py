5641a91cb31881d9195b4fd450e02a9580847957
Dennis Englund <dennisenglund88@live.com>
2020-09-17 10:16:56 +0200

    Added abit more tests
    Tests for specifially int/float and a mix added
    
    Co-Authored-By: ErikKrantz <25820542+ErikKrantz@users.noreply.github.com>

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -4,19 +4,33 @@ from ..utils import TranspileTestCase
 
 
 class MathModuleTests(TranspileTestCase):
-    def test_pow(self):
+    def test_pow_int_float_mix(self):
         self.assertCodeExecution("""
             import math
             print(math.pow(0, 2.0))
             print(math.pow(2, 2))
             print(math.pow(2.0, 2))
             print(math.pow(2, 2.0))
+        """)
+
+    def test_pow_int(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.pow(2, 2))
+            print(math.pow(0, 2))
+            print(math.pow(2, 0))
+            print(math.pow(0, 0))
+            print(math.pow(1, 2))
+            """)
+
+    def test_pow_float(self):
+        self.assertCodeExecution("""
+            import math
             print(math.pow(2.0, 2.0))
             print(math.pow(0.0, 2.0))
             print(math.pow(2.0, 0.0))
             print(math.pow(0.0, 0.0))
             print(math.pow(1.0, 2.0))
-            print(math.pow(2.0, 1.0))
         """)
 
     @unittest.expectedFailure
