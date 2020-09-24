a24d8c1d44d6137dda8794dc566c5f145b80454f
Erik Rimskog <erik.rimskog@gmail.com>
2020-09-24 16:00:28 +0200

    added unit tests for datetime

--- jtests/DateTimeTest.java
+++ jtests/DateTimeTest.java
@@ -1,102 +1,111 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.python.stdlib.datetime.DateTime;
 import org.python.exceptions.ValueError;
+import org.python.exceptions.TypeError;
+import org.python.types.Int;
 import java.util.Map;
 import java.util.HashMap;
 
 public class DateTimeTest {
     @Test
     public void testCreateArgs() {
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
         org.python.Object[] args = {
-            org.python.Int(1),
-            org.python.Int(2),
-            org.python.Int(3),
+            Int.getInt(1),
+            Int.getInt(2),
+            Int.getInt(3),
         };
-        DateTime dt = new DateTime(args);
-        assertEquals(dt.year, org.python.Int(1));
-        assertEquals(dt.month, org.python.Int(2));
-        assertEquals(dt.day, org.python.Int(3));
-        assertEquals(dt.hour, org.python.Int(0));
-        assertEquals(dt.minute, org.python.Int(0));
-        assertEquals(dt.second, org.python.Int(0));
-        assertEquals(dt.microsecond, org.python.Int(0));
+        DateTime dt = new DateTime(args, kwargs);
+        assertEquals(dt.year, Int.getInt(1));
+        assertEquals(dt.month, Int.getInt(2));
+        assertEquals(dt.day, Int.getInt(3));
+        assertEquals(dt.hour, Int.getInt(0));
+        assertEquals(dt.minute, Int.getInt(0));
+        assertEquals(dt.second, Int.getInt(0));
+        assertEquals(dt.microsecond, Int.getInt(0));
     }
 
     @Test
     public void testCreateKwargs() {
-        Map kwargs = new HashMap<java.lang.String, org.python.Object>();
-        kwargs.put("year", org.python.Int(1997));
-        kwargs.put("month", org.python.Int(12));
-        kwargs.put("day", org.python.Int(2));
-        kwargs.put("hour", org.python.Int(5));
-        kwargs.put("microseconds", org.python.Int(250000));
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        org.python.Object[] args = {};
+        kwargs.put("year", Int.getInt(1997));
+        kwargs.put("month", Int.getInt(12));
+        kwargs.put("day", Int.getInt(2));
+        kwargs.put("hour", Int.getInt(5));
+        kwargs.put("microsecond", Int.getInt(250000));
 
-        DateTime dt = new DateTime(kwargs);
-        assertEquals(dt.year, org.python.Int(1997));
-        assertEquals(dt.month, org.python.Int(12));
-        assertEquals(dt.day, org.python.Int(2));
-        assertEquals(dt.hour, org.python.Int(5));
-        assertEquals(dt.minute, org.python.Int(0));
-        assertEquals(dt.second, org.python.Int(0));
-        assertEquals(dt.microsecond, org.python.Int(250000));
+        DateTime dt = new DateTime(args, kwargs);
+        assertEquals(dt.year, Int.getInt(1997));
+        assertEquals(dt.month, Int.getInt(12));
+        assertEquals(dt.day, Int.getInt(2));
+        assertEquals(dt.hour, Int.getInt(5));
+        assertEquals(dt.minute, Int.getInt(0));
+        assertEquals(dt.second, Int.getInt(0));
+        assertEquals(dt.microsecond, Int.getInt(250000));
     }
 
-    @Test(expected = ValueError.class)
+    @Test(expected = TypeError.class)
     public void testCreateKwargsTooFew() {
-        Map kwargs = new HashMap<java.lang.String, org.python.Object>();
-        kwargs.put("year", org.python.Int(1997));
-        DateTime dt = new DateTime(kwargs);
+        org.python.Object[] args = {};
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        kwargs.put("year", Int.getInt(1997));
+        DateTime dt = new DateTime(args, kwargs);
     }
 
-    @Test(expected = ValueError.class)
+    @Test(expected = TypeError.class)
     public void testCreateKwargsTooFew2() {
-        Map kwargs = new HashMap<java.lang.String, org.python.Object>();
-        DateTime dt = new DateTime(kwargs);
+        org.python.Object[] args = {};
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        DateTime dt = new DateTime(args, kwargs);
     }
 
-    @Test(expected = ValueError.class)
+    @Test(expected = TypeError.class)
     public void testCreateArgsTooFew() {
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
         org.python.Object[] args = {
-            org.python.Int(1),
-            org.python.Int(2),
+            Int.getInt(1),
+            Int.getInt(2),
         };
-        DateTime dt = new DateTime(args);
+        DateTime dt = new DateTime(args, kwargs);
     }
 
-    @Test(expected = ValueError.class)
+    @Test(expected = TypeError.class)
     public void testCreateArgsTooFew2() {
         org.python.Object[] args = {};
-        DateTime dt = new DateTime(args);
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        DateTime dt = new DateTime(args, kwargs);
     }
 
     @Test()
     public void testCreateKwargsOutOfRange() {
-        Map kwargs = new HashMap<java.lang.String, org.python.Object>();
-        kwargs.put("year", org.python.Int(1997));
-        kwargs.put("month", org.python.Int(12));
-        kwargs.put("day", org.python.Int(1));
-        kwargs.put("hour", org.python.Int(1));
-        kwargs.put("minute", org.python.Int(1));
-        kwargs.put("second", org.python.Int(1));
-        kwargs.put("microsecond", org.python.Int(200000));
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        org.python.Object[] args = {};
+        kwargs.put("year", Int.getInt(1997));
+        kwargs.put("month", Int.getInt(12));
+        kwargs.put("day", Int.getInt(1));
+        kwargs.put("hour", Int.getInt(1));
+        kwargs.put("minute", Int.getInt(1));
+        kwargs.put("second", Int.getInt(1));
+        kwargs.put("microsecond", Int.getInt(200000));
 
-        Map faultykwargs = new HashMap<java.lang.String, org.python.Object>();
-        faultykwargs.put("year", org.python.Int(0));
-        faultykwargs.put("month", org.python.Int(0));
-        faultykwargs.put("day", org.python.Int(0));
-        faultykwargs.put("hour", org.python.Int(-1));
-        faultykwargs.put("minute", org.python.Int(-1));
-        faultykwargs.put("second", org.python.Int(-1));
-        faultykwargs.put("microsecond", org.python.Int(-1));
+        Map<String, org.python.Object> faultykwargs = new HashMap<String, org.python.Object>();
+        faultykwargs.put("year", Int.getInt(0));
+        faultykwargs.put("month", Int.getInt(0));
+        faultykwargs.put("day", Int.getInt(0));
+        faultykwargs.put("hour", Int.getInt(-1));
+        faultykwargs.put("minute", Int.getInt(-1));
+        faultykwargs.put("second", Int.getInt(-1));
+        faultykwargs.put("microsecond", Int.getInt(-1));
 
-        for (Map.Entry<_,_> entry : faultykwargs.entrySet()) {
+        for (Map.Entry<String,org.python.Object> entry : faultykwargs.entrySet()) {
             org.python.Object old = kwargs.get(entry.getKey());
             kwargs.put(entry.getKey(), entry.getValue());
-            bool threw = false;
+            boolean threw = false;
             try {
-                DateTime dt = new DateTime(kwargs);
-            } catch (org.python.exceptions.ValueError e) {
+                DateTime dt = new DateTime(args, kwargs);
+            } catch (ValueError e) {
                 //TODO: check exception message
                 threw = true;
             }
@@ -104,22 +113,22 @@ public class DateTimeTest {
             kwargs.put(entry.getKey(), old);
         }
 
-        faultykwargs.put("year", org.python.Int(10000));
-        faultykwargs.put("month", org.python.Int(13));
-        faultykwargs.put("day", org.python.Int(32));
-        faultykwargs.put("hour", org.python.Int(25));
-        faultykwargs.put("minute", org.python.Int(61));
-        faultykwargs.put("second", org.python.Int(61));
-        faultykwargs.put("microsecond", org.python.Int(1000001));
+        faultykwargs.put("year", Int.getInt(10000));
+        faultykwargs.put("month", Int.getInt(13));
+        faultykwargs.put("day", Int.getInt(32));
+        faultykwargs.put("hour", Int.getInt(25));
+        faultykwargs.put("minute", Int.getInt(61));
+        faultykwargs.put("second", Int.getInt(61));
+        faultykwargs.put("microsecond", Int.getInt(1000001));
 
         //TODO: DRY
-        for (Map.Entry<_,_> entry : faultykwargs.entrySet()) {
+        for (Map.Entry<String,org.python.Object> entry : faultykwargs.entrySet()) {
             org.python.Object old = kwargs.get(entry.getKey());
             kwargs.put(entry.getKey(), entry.getValue());
-            bool threw = false;
+            boolean threw = false;
             try {
-                DateTime dt = new DateTime(kwargs);
-            } catch (org.python.exceptions.ValueError e) {
+                DateTime dt = new DateTime(args, kwargs);
+            } catch (ValueError e) {
                 //TODO: check exception message
                 threw = true;
             }
