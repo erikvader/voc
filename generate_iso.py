40f5552744fb83be8a4b65870bd3a97f4f576cef
Niklas Persson <niklas.persson@gmail.com>
2020-10-07 15:49:43 +0200

    Load isos from file

--- generate_iso.py
+++ generate_iso.py
@@ -10,3 +10,9 @@ def generate_isos(count):
         iso = d.isoformat()
         if len(iso) == 26:
             yield iso
+
+
+if __name__ == "__main__":
+    n = 250001
+    with open('isos-%d.txt' % n, 'w+') as f:
+        f.write(','.join(generate_isos(n)))
