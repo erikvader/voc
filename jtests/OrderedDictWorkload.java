244085fd58a474f00863198064d30205ec8601d1
Erik Rimskog <erik.rimskog@gmail.com>
2020-10-12 15:20:37 +0200

    initial

---
+++ jtests/OrderedDictWorkload.java
@@ -0,0 +1,15 @@
+import org.python.stdlib.collections.OrderedDict;
+import org.python.types.Int;
+import java.util.HashMap;
+
+public class OrderedDictWorkload {
+    public static void main(String[] argssss) {
+        org.python.Object[] args = { null };
+        HashMap<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        OrderedDict dict = new OrderedDict(args, kwargs);
+
+        for(int i = 0; i < 1000000; i++){
+            dict.__setitem__(Int.getInt(i), Int.getInt(i));
+        }
+    }
+}
