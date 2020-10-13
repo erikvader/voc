f236e9ec28bdd721777a858bd6422bf2c18ed18b
nystrand1 <filip.nystrand@gmail.com>
2020-10-13 11:24:33 +0200

    Change to 0.5m inserts, 0.5 deletions

--- jtests/OrderedDictWorkload.java
+++ jtests/OrderedDictWorkload.java
@@ -8,8 +8,12 @@ public class OrderedDictWorkload {
         HashMap<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
         OrderedDict dict = new OrderedDict(args, kwargs);
 
-        for(int i = 0; i < 1000000; i++){
+        for(int i = 0; i < 500000; i++){
             dict.__setitem__(Int.getInt(i), Int.getInt(i));
+        }
+
+        for(int i = 0; i < 500000; i++){
+            dict.__delitem__(dict.get(Int.getInt(i), Int.getInt(i)));
         }
     }
 }
